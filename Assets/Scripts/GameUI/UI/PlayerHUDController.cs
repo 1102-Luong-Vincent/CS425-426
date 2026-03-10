4faeff66be37cdcf73dadc4f9da71193339f9fbb
1102-Luong-Vincent <vinceluong2@gmail.com>
2026-03-10 11:07:46 -0700

    added ui for game start scene

---
+++ Assets/Scripts/GameUI/UI/PlayerHUDController.cs
@@ -0,0 +1,38 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHUDController : MonoBehaviour
+{
+    public Image playerIcon;        // portrait icon
+    public Slider healthBar;        // health bar UI
+    public TMP_Text healthText;     // health text
+
+    PlayerValue player;
+
+    void Start()
+    {
+        player = GameValue.Instance.playerValue;
+
+        healthBar.maxValue = player.GetMaxHealth();
+        UpdateHUD();
+    }
+
+    void Update()
+    {
+        UpdateHUD();
+    }
+
+    void UpdateHUD()
+    {
+        int currentHealth = player.GetHealth();
+        int maxHealth = player.GetMaxHealth();
+
+        healthBar.value = currentHealth;
+
+        if (healthText != null)
+        {
+            healthText.text = currentHealth + " / " + maxHealth;
+        }
+    }
+}
