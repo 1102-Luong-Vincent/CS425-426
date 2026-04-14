cbe97656e25dc93d0389ccce73138340f0e1625e
Shawn <144489041+1106-Meng-Shawn@users.noreply.github.com>
2026-04-14 11:14:12 -0700

    better interactable

--- Assets/Scripts/Interaction/InteractableNotification.cs
+++ Assets/Scripts/Interaction/InteractableNotification.cs
@@ -17,10 +17,6 @@ public class InteractableNotification : MonoBehaviour
     [SerializeField] public Image pickupIcon;
     [SerializeField] public float fadeDuration = 2f;
 
-    [Header("Resource Drops")]
-    [SerializeField] public TextMeshProUGUI resourceText;
-    [SerializeField] public Image resourceIcon;
-
     private Coroutine Notification;
     public void Awake()
     {
@@ -34,18 +30,16 @@ public class InteractableNotification : MonoBehaviour
 
         notificationText.gameObject.SetActive(false);
         pickupIcon.gameObject.SetActive(false);
-        resourceText.gameObject.SetActive(false);
-        resourceIcon.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
-    public void ShowNotification(string itemName, Sprite icon = null)
+    public void ShowNotification(string itemName, Sprite icon = null, int amount = 1)
     {
         if (Notification != null)
             StopCoroutine(Notification);
 
         //StopAllCoroutines();
-        notificationText.text = itemName + " x1";
+        notificationText.text = $"{itemName} x{amount}";
 
         notificationText.alpha = 1f;
         notificationText.gameObject.SetActive(true);
@@ -70,27 +64,7 @@ public class InteractableNotification : MonoBehaviour
 
     public void ShowResourceNotification(string resourceName, Sprite icon = null, int amount = 1)
     {
-        if (Notification != null)
-            StopCoroutine(Notification);
-
-        resourceText.text = $"{resourceName} x{amount}";
-        resourceText.alpha = 1f;
-        resourceText.gameObject.SetActive(true);
-
-        if (icon != null)
-        {
-            resourceIcon.sprite = icon;
-            Color c = resourceIcon.color;
-            c.a = 1f;
-            resourceIcon.color = c;
-            resourceIcon.gameObject.SetActive(true);
-        }
-        else
-        {
-            resourceIcon.gameObject.SetActive(false);
-        }
-
-        Notification = StartCoroutine(HideResourceAfterDuration());
+        ShowNotification(resourceName, icon, amount);
     }
 
     private IEnumerator HideAfterDuration()
@@ -119,32 +93,5 @@ public class InteractableNotification : MonoBehaviour
 
         notificationText.gameObject.SetActive(false);
         pickupIcon.gameObject.SetActive(false);
-    }
-
-    private IEnumerator HideResourceAfterDuration()
-    {
-        yield return new WaitForSeconds(displayDuration);
-
-        float elapsed = 0f;
-
-        while (elapsed < fadeDuration)
-        {
-            elapsed += Time.deltaTime;
-            float alpha = Mathf.Lerp(1f, 0f, elapsed / fadeDuration);
-
-            resourceText.alpha = alpha;
-
-            if (resourceIcon != null)
-            {
-                Color c = resourceIcon.color;
-                c.a = alpha;
-                resourceIcon.color = c;
-            }
-
-            yield return null;
-        }
-
-        resourceText.gameObject.SetActive(false);
-        resourceIcon.gameObject.SetActive(false);
     }
 }
